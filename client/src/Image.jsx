4ed42367ff16156043b74ecb73a18e2a780c64ba
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-28 00:12:30 +0800

    change envt

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,10 +1,7 @@
 export default function Image({src,...rest}) {
-    const baseUrl = process.env.VITE_API_BASE_URL;
     src = src && src.includes('https://')
       ? src
       :'http://localhost:4000/uploads/'+src;
-      
-
     console.log('src: ',src)
     return (
       <img {...rest} src={src} alt={''} />
