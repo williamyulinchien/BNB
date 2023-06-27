78fe70127fba18a3634f43aacdacdc28b3ec3fb8
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-27 22:55:06 +0800

    modify

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,9 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:4000/uploads/'+src;
+      : src;
+
+    console.log('src: ',src)
     return (
       <img {...rest} src={src} alt={''} />
     );
