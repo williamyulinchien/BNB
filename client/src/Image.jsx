f348de5eb26abb77a014341a3ac0409f89c80fd5
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-27 23:30:55 +0800

    change

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -2,7 +2,7 @@ export default function Image({src,...rest}) {
     const baseUrl = process.env.VITE_API_BASE_URL;
     src = src && src.includes('https://')
       ? src
-      :`${baseUrl}/uploads/${src}`;
+      :'http://localhost:4000/uploads/'+src;
       
 
     console.log('src: ',src)
