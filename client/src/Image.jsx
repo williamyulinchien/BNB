1bb26b347ef7672a14cb29dd995f818ecbbea75c
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-27 23:17:13 +0800

    try

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,9 @@
 export default function Image({src,...rest}) {
+    const baseUrl = process.env.VITE_API_BASE_URL;
     src = src && src.includes('https://')
       ? src
-      : 'https://william-booking-app.vercel.app/'+src;
+      :`${baseUrl}/uploads/${src}`;
+      
 
     console.log('src: ',src)
     return (
