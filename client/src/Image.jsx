cf85c1b0bfc0dc646af924f9a49e1d95dd3ccd52
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-29 10:55:29 +0800

    src issue

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,12 +1,14 @@
 export default function Image({src,...rest}) {
-  const domain = process.env.NODE_ENV === 'production' 
+    const domain = process.env.NODE_ENV === 'production' 
     ? 'https://bnb-theta.vercel.app/uploads/'
     : 'http://localhost:4000/uploads/';
-  src = src && src.includes('https://')
-      ? 'https://bnb-theta.vercel.app/uploads/'+src
-      :'http://localhost:4000/uploads/'+src;
-    console.log('src: ',src)
+
+    src = src && src.includes('https://')
+    ? src
+    : domain + src;
+
+    console.log('src: ',src);
     return (
       <img {...rest} src={src} alt={''} />
     );
-  }
+    }
