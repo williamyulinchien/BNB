d331597eb16d1d6383021ae2850b1884de6da426
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-06-29 10:45:15 +0800

    src issue

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,5 +1,8 @@
 export default function Image({src,...rest}) {
-    src = src && src.includes('https://')
+  const domain = process.env.NODE_ENV === 'production' 
+    ? 'https://your-vercel-domain.com/uploads/'
+    : 'http://localhost:4000/uploads/';
+  src = src && src.includes('https://')
       ? src
       :'http://localhost:4000/uploads/'+src;
     console.log('src: ',src)
